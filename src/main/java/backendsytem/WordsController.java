843e0f2c6066862db8643ac7d365de0a88b97662
Susana A. Rodriguez <surod22@gmail.com>
2016-01-11 07:47:06 -0500

    [Susana] Adding wro files. Modules for home and login endpoints.

--- src/main/java/backendsytem/WordsController.java
+++ src/main/java/backendsytem/WordsController.java
@@ -1,31 +1,41 @@
 package backendsytem;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
-@Controller
+@RestController
+@EnableAutoConfiguration
 public class WordsController {
 
+    @RequestMapping(value="/main")
+    public HashMap<String, Object> home(){
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("id", "1234");
+        model.put("content", "Hello world");
+        return model;
+    }
     @RequestMapping(value = "/words/avg_len", method= RequestMethod.POST,
             produces = "application/json",
             consumes = "application/json")
     @ResponseBody
-    public ResponseEntity<?> avglenHandler(@RequestBody Words words) throws IOException {
+    public ResponseEntity<Words> avglenHandler(@RequestBody Words words) throws IOException {
         WordService wordService = new WordService();
         words.setAvgLength(wordService.averageWordLength(words.getText()));
+        HashMap<String, Integer> model = new HashMap<String, Integer>();
+        model.put("avgLength", words.getAvgLength());
+        //return model;
         return new ResponseEntity<Words>(words, HttpStatus.ACCEPTED);
+
     }
+
+
 
     @RequestMapping(value = "/words/most_com", method= RequestMethod.POST,
             produces = "application/json",
--- src/main/resources/static/js/sigfigs.js
+++ src/main/resources/static/js/sigfigs.js
@@ -1,6 +1,43 @@
-angular.module('sigfigs', [])
-.controller('home', function($scope){
-        $http.post('/words/avg_len').success(function(data)){
-            $scope.words = data;
-        }
+angular.module('sigfigs', ['ngRoute'])
+    .config(function($routeProvider, $httpProvider){
+
+        $routeProvider.when('/', {
+            templateUrl : 'home.html'
+            controller : 'home'
+        }).when('/login', {
+            templateUrl : 'login.html',
+            controller : 'navigation'
+        }).otherwise('/');
+
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+    })
+.controller('home', function($scope, $http){
+    $http.get('/main/').success(function(data){
+        $scope.message = data; 
+    })
 })
+.controller('navigation',
+        function( $scope, $http, $location){
+
+
+        $scope.submitForm = function(){
+            $http.post({'/words/avg_len',
+                {   text : $scope.words.text},
+                {'Content-Type': 'application/json'}
+            })
+            .then(function(results){
+                console.log('mid', results);
+
+            }).catch(function(response){
+                console.log('Error', response.data.errors);
+            })
+        }
+        });
+
+        //var authenticate = function(words, callback){
+        //    $http.post('/words/avg_len').success(function(data)){
+        //                $scope.text = data.text;
+        //                $scope.avglen = data.avgLength;
+        //    }
+        //}
+
